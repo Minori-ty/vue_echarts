0354740d78d8c6d5e7c46814fc87ab27c9726b49
守夜人 <1229248506@qq.com>
2021-03-09 23:04:34 +0800

    初始化项目

---
+++ src/components/Seller.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      这里是seller的组件
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
---
+++ src/views/SellerPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <seller></seller>
+  </div>
+</template>
+<script>
+import Seller from '@/components/Seller'
+export default {
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {},
+  components: {
+    seller: Seller
+  }
+}
+</script>
+<style lang="less" scoped></style>
