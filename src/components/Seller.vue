673c9b26f0b7e1b93efd824231aeb62e4974a95d
守夜人 <1229248506@qq.com>
2021-03-10 22:17:56 +0800

    完成了商家销量统计图表

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -4,45 +4,155 @@
   </div>
 </template>
 <script>
-var echarts = require('echarts')
+// var echarts = require('echarts')
 export default {
   data () {
     return {
       chartInstance: null,
-      allData: null
+      allData: null,
+      currentPage: 1,
+      totalPage: 0,
+      timerId: null
     }
   },
   created () {},
   mounted () {
     this.initChart()
     this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
+  },
+  destroyed () {
+    clearInterval(this.timerId)
+    window.removeEventListener('resize', this.screenAdapter)
   },
   methods: {
     initChart () {
-      this.chartInstance = echarts.init(this.$refs.seller_ref)
-    },
-    async getData () {
-      const { data: res } = await this.$http.get('seller')
-      this.allData = res
-      this.updataChart()
-    },
-    updataChart () {
-      const sellerNames = this.allData.map(i => i.name)
-      const sellerValue = this.allData.map(i => i.value)
-      const option = {
+      this.chartInstance = this.$echarts.init(this.$refs.seller_ref, 'chalk')
+      const initOption = {
+        title: {
+          text: '| 商家销售统计',
+
+          left: 20,
+          top: 20
+        },
+        grid: {
+          top: '20%',
+          left: '3%',
+          right: '6%',
+          bottom: '3%',
+          containLabel: true
+        },
         xAxis: { type: 'value' },
         yAxis: {
-          type: 'category',
-          data: sellerNames
+          type: 'category'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            z: 0,
+            lineStyle: {
+              color: '#2D3443'
+            }
+          }
         },
         series: [
           {
             type: 'bar',
+
+            label: {
+              show: true,
+              position: 'right',
+              textStyle: {
+                color: 'white'
+              }
+            },
+            itemStyle: {
+              color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                { offset: 0, color: '#5052EE' },
+                { offset: 1, color: '#AB6EE5' }
+              ])
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(initOption)
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
+    },
+    async getData () {
+      const { data: res } = await this.$http.get('seller')
+      this.allData = res
+      this.allData.sort((a, b) => a.value - b.value)
+      this.totalPage = this.allData.length % 5 === 0 ? this.allData.length / 5 : this.allData.length / 5 + 1
+      this.updataChart()
+      this.startInterval()
+    },
+    updataChart () {
+      const start = (this.currentPage - 1) * 5
+      const end = this.currentPage * 5
+      const showData = this.allData.slice(start, end)
+      const sellerNames = showData.map(i => i.name)
+      const sellerValue = showData.map(i => i.value)
+      const dataOption = {
+        yAxis: {
+          data: sellerNames
+        },
+        series: [
+          {
             data: sellerValue
           }
         ]
       }
-      this.chartInstance.setOption(option)
+      this.chartInstance.setOption(dataOption)
+    },
+    startInterval () {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.currentPage++
+        if (this.currentPage >= this.totalPage) {
+          this.currentPage = 1
+        }
+        this.updataChart()
+      }, 2000)
+    },
+    screenAdapter () {
+      // console.log(this.$refs.seller_ref.offsetWidth)
+      const titleFontSize = (this.$refs.seller_ref.offsetWidth / 100) * 3.6
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+
+        tooltip: {
+          axisPointer: {
+            lineStyle: {
+              width: titleFontSize
+            }
+          }
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+
+            itemStyle: {
+              barBorderRadius: [0, titleFontSize / 2, titleFontSize / 2, 0]
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(adapterOption)
+      // 手动调用图表对象的resize才能产生效果
+      this.chartInstance.resize()
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import store from './store'
 import './mock/index.js'
 import axios from 'axios'
 import './assets/css/global.css'
+// import * as echarts from 'echarts'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/'
 
+Vue.prototype.$echarts = window.echarts
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
